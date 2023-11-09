2ac87cc996e5764bd563a7b921ea5b7b2d6487a2
aavetisyanit <aavetisyanit@gmail.com>
2023-11-08 21:48:16 -0600

    Udpate

--- controller.js
+++ controller.js
@@ -2,13 +2,19 @@ import https from 'https';
 
 const start = Date.now();
 
-https
-	.request('https://www.google.com/', res => {
-		res.on('data', () => {});
-		res.on('end', () => {
-			console.log('====================================');
-			console.log(Date.now() - start);
-			console.log('====================================');
-		});
-	})
-	.end();
+function doRequest() {
+	https
+		.request('https://www.google.com/', res => {
+			res.on('data', () => {});
+			res.on('end', () => {
+				console.log(Date.now() - start);
+			});
+		})
+		.end();
+}
+
+doRequest();
+doRequest();
+doRequest();
+doRequest();
+doRequest();

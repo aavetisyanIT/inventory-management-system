6b918f7789112603cebf7c92f9b39eb9fba860e4
aavetisyanit <aavetisyanit@gmail.com>
2023-12-03 11:17:19 -0600

    Define Field class and add print method

--- starting/main.js
+++ starting/main.js
@@ -1,6 +1,24 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require('prompt-sync')({ sigint: true });
 
 const hat = '^';
 const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
+
+class Field {
+	constructor(field) {
+		this.field = field;
+	}
+
+	print() {
+		this.field.forEach(row => console.log(row.join('')));
+	}
+}
+
+const myField = new Field([
+	[pathCharacter, fieldCharacter, hole],
+	[fieldCharacter, hole, fieldCharacter],
+	[fieldCharacter, hat, fieldCharacter],
+]);
+
+myField.print();

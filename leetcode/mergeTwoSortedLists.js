c2d6284b24b353b313d6de8f03846ef1080755f9
aavetisyanit <aavetisyanit@gmail.com>
2023-12-12 16:02:56 -0600

    Add logic that merges two sorted linked lists

---
+++ leetcode/mergeTwoSortedLists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+	let currentNode = new ListNode();
+	const mergedList = currentNode;
+
+	while (list1 && list2) {
+		if (list1.val < list2.val) {
+			currentNode.next = list1;
+			list1 = list1.next;
+		} else {
+			currentNode.next = list2;
+			list2 = list2.next;
+		}
+		currentNode = currentNode.next;
+	}
+	//add last element
+	if (list1) currentNode.next = list1;
+	if (list2) currentNode.next = list2;
+	return mergedList.next;
+};

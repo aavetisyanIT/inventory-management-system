5ca3126179c5f3acceffec6d9f4a699b2a4caf60
aavetisyanit <aavetisyanit@gmail.com>
2023-12-13 15:34:43 -0600

    Complete constraint words logic

--- main.js
+++ main.js
@@ -11,17 +11,22 @@ const url = 'https://api.datamuse.com/words?sl=';
 const getSuggestions = () => {
 	const wordQuery = inputField.value;
 	const endpoint = url + wordQuery;
-	fetch(endpoint, { cache: 'no-cache' }).then(
-		response => {
-			if (response.ok) {
-				return response.json();
-			}
-			throw new Error('Request failed!');
-		},
-		networkError => {
-			console.log(networkError.message);
-		},
-	);
+	fetch(endpoint, { cache: 'no-cache' })
+		.then(
+			response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Request failed!');
+			},
+			networkError => {
+				console.log(networkError.message);
+			},
+		)
+		.then(jsonResponse => {
+			// renderRawResponse(jsonResponse);
+			renderResponse(jsonResponse);
+		});
 };
 
 // Clears previous results and display results to webpage

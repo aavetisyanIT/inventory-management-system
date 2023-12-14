81fcd7e56a15127082215f3ec9af114025bcb4b2
aavetisyanit <aavetisyanit@gmail.com>
2023-12-14 15:22:32 -0600

    Add POST request logic

--- main.js
+++ main.js
@@ -39,3 +39,44 @@ const displaySuggestions = event => {
 };
 
 submit.addEventListener('click', displaySuggestions);
+
+// Asynchronous functions
+const shortenUrl = () => {
+	const urlToShorten = inputField.value;
+	const data = JSON.stringify({ destination: urlToShorten });
+
+	fetch(url, {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json',
+			apikey: apiKey,
+		},
+		body: data,
+	})
+		.then(
+			response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error('Request failed!');
+				throw new Error('reach failed!');
+			},
+			networkError => {
+				console.log(networkError.message);
+			},
+		)
+		.then(jsonResponse => {
+			renderResponse(jsonResponse);
+		});
+};
+
+// Clear page and call Asynchronous functions
+const displayShortUrl = event => {
+	event.preventDefault();
+	while (responseField.firstChild) {
+		responseField.removeChild(responseField.firstChild);
+	}
+	shortenUrl();
+};
+
+shortenButton.addEventListener('click', displayShortUrl);

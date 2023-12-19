fbe5514f661a21da21d1097f9b40d2bd63423c75
aavetisyanit <aavetisyanit@gmail.com>
2023-12-19 16:52:36 -0600

    Add controller for getbyid requests

--- express/expressions/app.js
+++ express/expressions/app.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const app = express();
-const { seedElements } = require('./utils');
 
 // Serves Express Yourself website
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 4001;
+const { getElementById, seedElements } = require('./utils');
 
 const expressions = [];
 seedElements(expressions, 'expressions');
 
-// Get all expressions
+const PORT = process.env.PORT || 4001;
+
 app.get('/expressions', (req, res, next) => {
 	res.send(expressions);
+});
+
+// Add a new call to app.get('/expressions/:id') here
+app.get('/expressions/:id', (req, res, next) => {
+	const id = req.params.id;
+	const element = getElementById(id, expressions);
+	if (element) return res.send(element);
+	res.send('Wrong id!');
 });
 
 app.listen(PORT, () => {

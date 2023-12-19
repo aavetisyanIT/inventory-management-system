078dc6333a235e7c0c4cc43f6a27e5a5b65f5388
aavetisyanit <aavetisyanit@gmail.com>
2023-12-19 17:24:09 -0600

    Add logic to update elements by ID

--- express/expressions/app.js
+++ express/expressions/app.js
@@ -4,7 +4,12 @@ const app = express();
 // Serves Express Yourself website
 app.use(express.static('public'));
 
-const { getElementById, seedElements } = require('./utils');
+const {
+	getElementById,
+	getIndexById,
+	updateElement,
+	seedElements,
+} = require('./utils');
 
 const expressions = [];
 seedElements(expressions, 'expressions');
@@ -15,12 +20,23 @@ app.get('/expressions', (req, res, next) => {
 	res.send(expressions);
 });
 
-// Add a new call to app.get('/expressions/:id') here
 app.get('/expressions/:id', (req, res, next) => {
-	const id = req.params.id;
-	const element = getElementById(id, expressions);
-	if (element) return res.send(element);
-	res.send('Wrong id!');
+	const { id } = req.params;
+	const foundExpression = getElementById(id, expressions);
+	const index = getIndexById(id, expressions);
+	if (foundExpression) {
+		res.send(foundExpression);
+	} else {
+		res.status(404).send();
+	}
+});
+
+app.put('/expressions/:id', (req, res, next) => {
+	const { id } = req.params;
+	const index = getIndexById(id, expressions);
+	if (index === -1) return res.status(404).send('ID is invalid!');
+	const updatedElement = updateElement(id, req.query, expressions);
+	res.send(updatedElement);
 });
 
 app.listen(PORT, () => {

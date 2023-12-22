e091e530bdd4e10c32b9bc330fd911ab2b0ac83e
aavetisyanit <aavetisyanit@gmail.com>
2023-12-22 07:36:53 -0600

    Start moving logic to routers

--- express/expressions/app.js
+++ express/expressions/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-// Serves Express Yourself website
-app.use(express.static('public'));
-
+const expressionsRouter = require('./expressions.js');
 const {
 	getElementById,
 	getIndexById,
@@ -12,17 +10,16 @@ const {
 	createElement,
 } = require('./utils');
 
-const expressions = [];
-seedElements(expressions, 'expressions');
+const PORT = process.env.PORT || 4001;
+// Use static server to serve the Express Yourself Website
+app.use(express.static('public'));
+
 const animals = [];
 seedElements(animals, 'animals');
 
-const PORT = process.env.PORT || 4001;
+app.use('/expressions', expressionsRouter);
 
-app.get('/expressions', (req, res, next) => {
-	res.send(expressions);
-});
-
+// Get a single expression
 app.get('/expressions/:id', (req, res, next) => {
 	const foundExpression = getElementById(req.params.id, expressions);
 	if (foundExpression) {
@@ -32,6 +29,7 @@ app.get('/expressions/:id', (req, res, next) => {
 	}
 });
 
+// Update an expression
 app.put('/expressions/:id', (req, res, next) => {
 	const expressionIndex = getIndexById(req.params.id, expressions);
 	if (expressionIndex !== -1) {
@@ -42,6 +40,7 @@ app.put('/expressions/:id', (req, res, next) => {
 	}
 });
 
+// Create an expression
 app.post('/expressions', (req, res, next) => {
 	const receivedExpression = createElement('expressions', req.query);
 	if (receivedExpression) {
@@ -52,6 +51,7 @@ app.post('/expressions', (req, res, next) => {
 	}
 });
 
+// Delete an expression
 app.delete('/expressions/:id', (req, res, next) => {
 	const expressionIndex = getIndexById(req.params.id, expressions);
 	if (expressionIndex !== -1) {
@@ -62,49 +62,54 @@ app.delete('/expressions/:id', (req, res, next) => {
 	}
 });
 
+// Get all animals
 app.get('/animals', (req, res, next) => {
-	res.status(200).send(animals);
+	res.send(animals);
 });
 
+// Get a single animal
 app.get('/animals/:id', (req, res, next) => {
 	const animal = getElementById(req.params.id, animals);
 	if (animal) {
 		res.send(animal);
-		return;
+	} else {
+		res.status(404).send();
 	}
-	res.status(404).send('Error');
 });
 
+// Create an animal
+app.post('/animals', (req, res, next) => {
+	const receivedAnimal = createElement('animals', req.query);
+	if (receivedAnimal) {
+		animals.push(receivedAnimal);
+		res.status(201).send(receivedAnimal);
+	} else {
+		res.status(400).send();
+	}
+});
+
+// Update an animal
 app.put('/animals/:id', (req, res, next) => {
 	const animalIndex = getIndexById(req.params.id, animals);
 	if (animalIndex !== -1) {
 		updateElement(req.params.id, req.query, animals);
 		res.send(animals[animalIndex]);
-		return;
+	} else {
+		res.status(404).send();
 	}
-	res.status(404).send('Error');
 });
 
-app.post('/animals', ({ query }, res, next) => {
-	const newAnimal = createElement('animals', query);
-	if (newAnimal) {
-		animals.push(newAnimal);
-		res.status(201).send(newAnimal);
-		return;
-	}
-	return res.status(400).send();
-});
-
-app.delete('/animals/:id', ({ params }, res, next) => {
-	const { id } = params;
-	const animalIndex = getIndexById(id, animals);
+// Delete a single animal
+app.delete('/animals/:id', (req, res, next) => {
+	const animalIndex = getIndexById(req.params.id, animals);
 	if (animalIndex !== -1) {
-		const deletedAnimal = animals.splice(animalIndex, 1);
-		res.status(204).send(deletedAnimal);
+		animals.splice(animalIndex, 1);
+		res.status(204).send();
+	} else {
+		res.status(404).send();
 	}
-	res.status(404).send();
 });
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+	console.log(`Server is listening on ${PORT}`);
 });

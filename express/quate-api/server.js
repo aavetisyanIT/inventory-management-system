9826dd5f698015176bc1f8177a4d5b12478ea454
aavetisyanit <aavetisyanit@gmail.com>
2023-12-24 07:19:15 -0600

    run server and define /api/quotes/random get route

--- express/quate-api/server.js
+++ express/quate-api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 const { quotes } = require('./data');
@@ -8,3 +9,15 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (_, res) => {
+	const randomQuote = getRandomElement(quotes);
+	if (randomQuote) {
+		res.send({ quote: randomQuote });
+		return;
+	}
+	res.status(404).send('Error');
+});
+
+app.listen(PORT, () => {
+	console.log('Server is running on port:', PORT);
+});

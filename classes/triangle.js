0c9d03fe808c5b0c57b537b49349ef561dbc88e7
aavetisyanit <aavetisyanit@gmail.com>
2024-01-11 21:27:05 -0600

    Instantiate Triangle class

--- classes/triangle.js
+++ classes/triangle.js
@@ -6,3 +6,11 @@ class Triangle {
 		return Math.sqrt(this.a ** 2 + this.b ** 2);
 	}
 }
+
+const myTry = new Triangle();
+myTry.a = 3;
+myTry.b = 5;
+
+console.log('====================================');
+console.log(myTry.getAria());
+console.log('====================================');

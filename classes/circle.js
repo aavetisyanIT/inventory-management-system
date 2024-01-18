41d25ad2b008c4beffcf21cd8c615c7e70d13e5c
aavetisyanit <aavetisyanit@gmail.com>
2024-01-18 16:07:53 -0600

    Define setter method with validation of colors

--- classes/circle.js
+++ classes/circle.js
@@ -1,12 +1,27 @@
 class Circle {
-	constructor(radius) {
+	static allowedColors = new Set(['red', 'green', 'black']);
+	constructor(radius, color) {
 		this.radius = radius;
+		this.setColor(color);
+	}
+
+	setColor(newColor) {
+		if (Circle.allowedColors.has(newColor)) {
+			this._color = newColor;
+		} else {
+			throw new Error('This color is not allowed');
+		}
 	}
 
 	get diameter() {
 		return this.radius * 2;
 	}
+
+	set color(newColor) {
+		this.setColor(newColor);
+	}
 }
 
-const myCircle = new Circle(3);
-console.log(myCircle.diameter);
+const myCircle = new Circle(3, 'red');
+myCircle.color = 'red';
+console.log(myCircle);

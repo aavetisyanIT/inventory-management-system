427c1496702500f3942a80b5bae01aba163edfd6
aavetisyanit <aavetisyanit@gmail.com>
2024-01-19 10:51:16 -0600

    Create logic for adding head and tail nodes

--- data_structures/doublyLinkedList_class.js
+++ data_structures/doublyLinkedList_class.js
@@ -6,6 +6,32 @@ export class DoublyLinkedList {
 		this.tail = null;
 	}
 
+	addToHead(data) {
+		const currentHead = this.head;
+		const newHead = new Node(data);
+
+		if (currentHead) {
+			this.head = newHead;
+			newHead.setNextNode(currentHead);
+			currentHead.setPreviousNode(newHead);
+		}
+		this.head = newHead;
+		if (!this.tail) this.tail = newHead;
+	}
+
+	addToTail(data) {
+		const currentTail = this.tail;
+		const newTail = new Node(data);
+
+		if (currentTail) {
+			this.tail = newTail;
+			newTail.setPreviousNode(currentTail);
+			currentTail.setNextNode(newTail);
+		}
+		this.tail = newTail;
+		if (!this.head) this.head = newTail;
+	}
+
 	printList() {
 		let currentNode = this.head;
 		let output = '<head> ';

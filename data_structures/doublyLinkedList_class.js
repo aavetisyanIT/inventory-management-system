b12a20c20070a7ad908d6b742b7f01d017b0ba23
aavetisyanit <aavetisyanit@gmail.com>
2024-01-19 12:14:36 -0600

    Add removeByData method to DoublyLinkedList

--- data_structures/doublyLinkedList_class.js
+++ data_structures/doublyLinkedList_class.js
@@ -32,6 +32,44 @@ export class DoublyLinkedList {
 		if (!this.head) this.head = newTail;
 	}
 
+	removeHead() {
+		const removedHead = this.head;
+		if (!removedHead) return;
+		this.head = removedHead.getNextNode();
+		if (this.head) this.head.setPreviousNode(null);
+		if (removedHead === this.tail) this.removeTail();
+		return removedHead.data;
+	}
+
+	removeTail() {
+		const removedTail = this.tail;
+		if (!removedTail) return;
+		this.tail = removedTail.getPreviousNode();
+		if (this.tail) this.tail.setNextNode(null);
+		if (removedTail === this.head) this.removeHead();
+		return removedTail.data;
+	}
+
+	removeByData(data) {
+		let nodeToRemove;
+		let currentNode = this.head;
+		while (currentNode) {
+			if (currentNode.data === data) {
+				nodeToRemove = currentNode;
+				break;
+			}
+			currentNode = currentNode.getNextNode();
+		}
+		if (!nodeToRemove) return nodeToRemove;
+		if (this.head === nodeToRemove) return this.removeHead();
+		if (this.tail === nodeToRemove) return this.removeTail();
+		const nextNode = nodeToRemove.getNextNode();
+		const previousNode = nodeToRemove.getPreviousNode();
+		nextNode.setPreviousNode(previousNode);
+		previousNode.setNextNode(nextNode);
+		return nodeToRemove;
+	}
+
 	printList() {
 		let currentNode = this.head;
 		let output = '<head> ';

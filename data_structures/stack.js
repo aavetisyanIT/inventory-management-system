6ff191e2f28ee239f8e85468a908b8294993f44d
aavetisyanit <aavetisyanit@gmail.com>
2024-01-21 15:33:45 -0600

    Change stack class to bounded

--- data_structures/stack.js
+++ data_structures/stack.js
@@ -1,19 +1,39 @@
 import { LinkedList } from './linked_list_class';
 
 export class Stack {
-	constructor() {
+	constructor(maxSize = Infinity) {
 		this.stack = new LinkedList();
+		this.maxSize = maxSize;
+		this.size = 0;
 	}
 
-	peek() {
-		return this.stack.head.data;
+	isEmpty() {
+		return this.size === 0;
+	}
+
+	hasRoom() {
+		return this.size < this.maxSize;
 	}
 
 	push(value) {
+		if (!this.hasRoom()) throw new Error('Stack is full');
 		this.stack.addToHead(value);
+		this.size++;
 	}
 
 	pop() {
-		return this.stack.removeHead();
+		if (!this.isEmpty()) {
+			const value = this.stack.removeHead();
+			this.size--;
+			return value;
+		} 
+    console.log('Stack is empty!');
+	}
+
+	peek() {
+		if (!this.isEmpty()) {
+			return this.stack.head.data;
+		}
+    return null;
 	}
 }

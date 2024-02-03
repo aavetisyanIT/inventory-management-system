a17cac04eeb2507b2da7919f611d42a80ee2d8a3
aavetisyanit <aavetisyanit@gmail.com>
2024-02-03 07:37:06 -0600

    Add hash method

--- data_structures/hashMap.js
+++ data_structures/hashMap.js
@@ -2,4 +2,12 @@ export class HashMap {
 	constructor(size = 0) {
 		this.hashMap = new Array(size).fill(null);
 	}
+
+	hash(key) {
+		let hashCode = 0;
+		for (let i = 0; i < key.length; i++) {
+			hashCode += hashCode + key.charCodeAt(i);
+		}
+		return hashCode;
+	}
 }

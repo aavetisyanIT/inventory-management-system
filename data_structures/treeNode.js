e01a61e7c3f708dacdc8bb3df3b822a5d5c91a6e
aavetisyanit <aavetisyanit@gmail.com>
2024-02-06 16:52:09 -0600

    Create removeChild method

--- data_structures/treeNode.js
+++ data_structures/treeNode.js
@@ -11,4 +11,18 @@ export class TreeNode {
 		}
 		this.children.push(new TreeNode(child));
 	}
+
+	removeChild(childToRemove) {
+		const length = this.children.length;
+		this.children = this.children.filter(child => {
+			if (childToRemove instanceof TreeNode) {
+				return childToRemove !== child;
+			}
+			return child.data !== childToRemove;
+		});
+
+		if (length === this.children.length) {
+			this.children.forEach(child => child.removeChild(childToRemove));
+		}
+	}
 }

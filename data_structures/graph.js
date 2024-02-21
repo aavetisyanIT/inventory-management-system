e3582c145a723aab46d4075b2166d6945bbf8f2d
aavetisyanit <aavetisyanit@gmail.com>
2024-02-20 21:35:07 -0600

    Work on Graph class

--- data_structures/graph.js
+++ data_structures/graph.js
@@ -2,6 +2,16 @@ import { Edge } from './edge';
 import { Vertex } from './vertex';
 
 export class Graph {
+	constructor() {
+		this.vertices = [];
+	}
+
+	addVertex(data) {
+		const newVertex = new Vertex(data);
+		this.vertices.push(newVertex);
+		return newVertex;
+	}
+
 	print() {
 		const vertexList = this.vertices || [];
 		vertexList.forEach(vertex => vertex.print());

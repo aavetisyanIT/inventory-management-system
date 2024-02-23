d0f696072ce715ca5bce6791da4d33403179bc1b
aavetisyanit <aavetisyanit@gmail.com>
2024-02-22 21:48:16 -0600

    Calculate factorial iteratively

---
+++ algorithms/factorial.js
@@ -0,0 +1,12 @@
+const iterativeFactorial = n => {
+	let result = 1;
+	while (n > 0) {
+		result *= n;
+		n -= 1;
+	}
+	return result;
+};
+
+// Set fourFactorial
+const fourFactorial = iterativeFactorial(4);
+console.log(fourFactorial);

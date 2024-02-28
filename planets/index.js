d36e6235b4b2caa55bdc229bea6fe1b63584a513
aavetisyanit <aavetisyanit@gmail.com>
2024-02-28 16:13:22 -0600

    Create stream for reading planet data

--- planets/index.js
+++ planets/index.js
@@ -1,30 +1,13 @@
 const { parse } = require('csv-parse');
 const fs = require('fs');
 
-const habitablePlanets = [];
-
-function isHabitablePlanet(planet) {
-  return planet['koi_disposition'] === 'CONFIRMED'
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
-}
+const result = [];
 
 fs.createReadStream('kepler_data.csv')
-  .pipe(parse({
-    comment: '#',
-    columns: true,
-  }))
-  .on('data', (data) => {
-    if (isHabitablePlanet(data)) {
-      habitablePlanets.push(data);
-    }
-  })
-  .on('error', (err) => {
-    console.log(err);
-  })
-  .on('end', () => {
-    console.log(habitablePlanets.map((planet) => {
-      return planet['kepler_name'];
-    }));
-    console.log(`${habitablePlanets.length} habitable planets found!`);
-  });
+	.on('data', data => {
+		result.push(data);
+	})
+	.on('error', err => console.log(err))
+	.on('end', () => {
+		console.log(result);
+	});

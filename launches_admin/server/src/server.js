77003eca69ada94824ede507e6905c3f81882379
aavetisyanit <aavetisyanit@gmail.com>
2024-03-04 14:51:42 -0600

    Update logic to load planet data asynchronously before server start

--- launches_admin/server/src/server.js
+++ launches_admin/server/src/server.js
@@ -1,8 +1,14 @@
 const http = require('http');
 const app = require('./app');
+const { loadPlanets } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
+async function startServer() {
+	await loadPlanets();
+	server.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
+}
+
+startServer();

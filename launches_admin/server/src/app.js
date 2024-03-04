83dff895bbc2b5420da9f43559f8312c951e011b
aavetisyanit <aavetisyanit@gmail.com>
2024-03-04 14:01:54 -0600

    Add CORS policies

--- launches_admin/server/src/app.js
+++ launches_admin/server/src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const planetsRouter = require('./routes/planetsRouter/planets.router');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(planetsRouter);
 
 module.exports = app;

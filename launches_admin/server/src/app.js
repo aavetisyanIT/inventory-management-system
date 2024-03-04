1bc0c3c938be9459731d6af142de6898b65e4ac4
aavetisyanit <aavetisyanit@gmail.com>
2024-03-04 16:08:53 -0600

    Set deploy script and start app from server

--- launches_admin/server/src/app.js
+++ launches_admin/server/src/app.js
@@ -1,11 +1,18 @@
+const path = require('path');
 const express = require('express');
-const planetsRouter = require('./routes/planetsRouter/planets.router');
 const cors = require('cors');
+
+const planetsRouter = require('./routes/planetsRouter/planets.router');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
+
 app.use(planetsRouter);
 
 module.exports = app;

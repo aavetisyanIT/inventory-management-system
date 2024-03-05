02c832f2d4835c193df030e65865385d65d2fbba
aavetisyanit <aavetisyanit@gmail.com>
2024-03-05 15:56:13 -0600

    Set up json and CORS middleware

--- mail_bag/server/src/main.ts
+++ mail_bag/server/src/main.ts
@@ -1,3 +1,20 @@
-const myString: string = 'Hello World';
+import path from 'path';
+import express, { Express, Request, Response, NextFunction } from 'express';
 
-console.log(myString);
+const app: Express = express();
+
+app.use(express.json());
+app.use(
+	'/',
+	express.static(path.join(__dirname, '..', '..', 'client', 'dist')),
+);
+app.use(function (req: Request, res: Response, next: NextFunction) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST, DELETE, OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept',
+	);
+
+	next();
+});

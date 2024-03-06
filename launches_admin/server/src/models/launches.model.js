d1c40dee900f39885b56f32330e464648f7885a9
aavetisyanit <aavetisyanit@gmail.com>
2024-03-05 22:29:47 -0600

    Define launches model and mock launch

---
+++ launches_admin/server/src/models/launches.model.js
@@ -0,0 +1,18 @@
+const launches = new Map();
+
+const launch = {
+	flightNumber: 100,
+	mission: 'Explore Space X',
+	rocket: 'Explorer IS1',
+	launchDate: new Date('December 20, 2040'),
+	destination: ' Kepler-1652 b',
+	customer: ['NASA'],
+	upcoming: true,
+	success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+module.exports = {
+	launches,
+};

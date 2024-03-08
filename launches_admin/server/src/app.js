cc5b17fa64175d81bfd115712a0e32cf30246b32
aavetisyanit <aavetisyanit@gmail.com>
2024-03-08 14:31:24 -0600

    Add catch-all route and fix the issue

--- launches_admin/server/src/app.js
+++ launches_admin/server/src/app.js
@@ -13,7 +13,7 @@ app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(morgan('short'));
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.get('/*', (req, res) => {
+app.all('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 

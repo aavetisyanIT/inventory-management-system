98e360b30a903f948402d1492a22e280ce271359
aavetisyanit <aavetisyanit@gmail.com>
2024-03-13 15:45:58 -0500

    Add validation for launch POST body params

--- launches_admin/server/src/routes/launchesRouter/launches.controller.js
+++ launches_admin/server/src/routes/launchesRouter/launches.controller.js
@@ -6,7 +6,22 @@ function httpGetAllLaunches(req, res) {
 
 function httpAddNewLaunch(req, res) {
 	const launch = req.body;
+	const { mission, rocket, launchDate, destination } = launch;
+
+	if (!mission || !rocket || !launchDate || !destination) {
+		return res.status(400).json({
+			error: 'Invalid body request',
+		});
+	}
+
 	launch.launchDate = new Date(launch.launchDate);
+
+	if (isNaN(launch.launchDate)) {
+		return res.status(400).json({
+			error: 'Request has invalid launchDate',
+		});
+	}
+
 	addNewLaunch(launch);
 
 	return res.status(201).json(launch);

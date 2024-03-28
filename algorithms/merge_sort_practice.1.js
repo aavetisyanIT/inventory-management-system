62fa7212b69777da2e168afaf5ff9b88eb1b97a5
aavetisyanit <aavetisyanit@gmail.com>
2024-03-28 15:31:09 -0500

    Work on partition function

---
+++ algorithms/merge_sort_practice.1.js
@@ -0,0 +1,16 @@
+const partition = (array, leftIndex, rightIndex) => {
+	const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
+	while (leftIndex <= rightIndex) {
+		while (array[leftIndex] < pivot) {
+			leftIndex++;
+		}
+
+		while (array[rightIndex] > pivot) {
+			rightIndex--;
+		}
+	}
+};
+
+module.exports = {
+	partition,
+};

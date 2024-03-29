6be91510895f8316c4744ae505cb160d09b2e07d
aavetisyanit <aavetisyanit@gmail.com>
2024-03-29 17:14:18 -0500

    Work on quick sort logic

--- algorithms/quick_sort_pracitce_1.js
+++ algorithms/quick_sort_pracitce_1.js
@@ -1,4 +1,24 @@
-const swap = require('./swap');
+const swap = (arr, indexOne, indexTwo) => {
+	const temp = arr[indexTwo];
+	arr[indexTwo] = arr[indexOne];
+	arr[indexOne] = temp;
+};
+
+export const quicksort = (
+	array,
+	leftBound = 0,
+	rightBound = array.length - 1,
+) => {
+	console.log('AAA leftBound', leftBound);
+	console.log('AAA rightBound', rightBound);
+
+	if (leftBound < rightBound) {
+		const pivotIndex = partition(array, leftBound, rightBound);
+		partition(array, leftBound, pivotIndex - 1);
+		partition(array, pivotIndex, rightBound);
+	}
+	return array;
+};
 
 const partition = (array, leftIndex, rightIndex) => {
 	const pivot = array[Math.floor((rightIndex + leftIndex) / 2)];
@@ -17,3 +37,8 @@ const partition = (array, leftIndex, rightIndex) => {
 	}
 	return leftIndex;
 };
+
+const array = [5, 3, 8, 1, 4, 2];
+console.log('Original array:', array);
+const sortedArray = quicksort(array);
+console.log('Sorted array:', sortedArray);

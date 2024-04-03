2658fe150be1772bd2fccf8770173f3d0962dce4
aavetisyanit <aavetisyanit@gmail.com>
2024-04-03 16:34:21 -0500

    Complete binary search implementation

--- algorithms/binary_search.js
+++ algorithms/binary_search.js
@@ -1,6 +1,28 @@
 const binarySearch = (arr, target) => {
 	let left = 0;
 	let right = arr.length;
-	const indexToCheck = Math.floor((left + right) / 2);
-	console.log(indexToCheck);
+
+	while (left < right) {
+		const indexToCheck = Math.floor((left + right) / 2);
+		const checking = arr[indexToCheck];
+		console.log(`indexToCheck equals: ${indexToCheck}`);
+
+		if (checking === target) {
+			return indexToCheck;
+		} else if (target < checking) {
+			right = indexToCheck;
+		} else {
+			left = indexToCheck + 1;
+		}
+	}
+	return null;
 };
+
+const searchable = [1, 2, 7, 8, 22, 28, 41, 58, 67, 71, 94];
+const target = 58;
+
+const targetIndex = binarySearch(searchable, target);
+
+console.log(`The target index is ${targetIndex}.`);
+
+module.exports = { binarySearch };

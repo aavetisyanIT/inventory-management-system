7f5e7f4a29fcd8c96a366889b460bd256576f520
aavetisyanit <aavetisyanit@gmail.com>
2024-04-05 15:32:23 -0500

    Define logic to add new value

--- data_structures/binary_search_tree.js
+++ data_structures/binary_search_tree.js
@@ -5,4 +5,18 @@ class BinaryTree {
 		this.left = null;
 		this.right = null;
 	}
+
+	insert(value) {
+		if (value < this.value) {
+			if (this.left) {
+				return this.left.insert(value);
+			}
+			this.left = new BinaryTree(value, this.depth + 1);
+		} else {
+			if (this.right) {
+				return this.right.insert(value);
+			}
+			this.right = new BinaryTree(value, this.depth + 1);
+		}
+	}
 }

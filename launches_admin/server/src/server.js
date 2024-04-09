62b9852a7c889229bacb1527f8411b6b1e5ab671
aavetisyanit <aavetisyanit@gmail.com>
2024-04-09 17:19:12 -0500

    Connect to data base

--- launches_admin/server/src/server.js
+++ launches_admin/server/src/server.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const app = require('./app');
+const mongoose = require('mongoose');
+
 const { loadPlanets } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+const DB_connection_string = process.env.DB_CONNECTION_STRING;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => console.log('DB is connected'));
+mongoose.connection.on('error', err => console.error(err));
+
 async function startServer() {
+	await mongoose.connect(DB_connection_string);
 	await loadPlanets();
 	server.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
 }

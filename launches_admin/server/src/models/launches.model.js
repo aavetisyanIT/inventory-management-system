8ad9efa1f9d719022166da8e727e4645f39f91c8
aavetisyanit <aavetisyanit@gmail.com>
2024-07-23 15:47:19 -0500

    Fix sorting issue for pagination

--- launches_admin/server/src/models/launches.model.js
+++ launches_admin/server/src/models/launches.model.js
@@ -90,7 +90,10 @@ async function getLatestFlightNumber() {
 }
 
 async function getAllLaunches(skip, limit) {
-	return await LaunchModel.find({}, { _id: 0, __v: 0 }).skip(skip).limit(limit);
+	return await LaunchModel.find({}, { _id: 0, __v: 0 })
+		.sort({ flightNumber: 1 })
+		.skip(skip)
+		.limit(limit);
 }
 
 async function getLaunchById(id) {

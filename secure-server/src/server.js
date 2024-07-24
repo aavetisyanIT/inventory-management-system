5afca09dcb617ba593cd2d90264cc16f3d93ef7d
aavetisyanit <aavetisyanit@gmail.com>
2024-07-24 13:25:28 -0500

    add tls certificate

--- secure-server/src/server.js
+++ secure-server/src/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const path = require('path');
+const fs = require('fs');
+
+const privateKey = fs.readFileSync(__dirname + '/../key.pem');
+const certificate = fs.readFileSync(__dirname + '/../cert.pem');
 
 const app = express();
+
+const credentials = { key: privateKey, cert: certificate };
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, '../index.html'));
 });
-const server = http.createServer(app);
+const server = https.createServer(credentials, app);
 
 server.listen(3030, () => console.log('App is serving on port 3030'));

1957a8a084a79c4428129e1aae44e96cc6d66052
aavetisyanit <aavetisyanit@gmail.com>
2024-07-25 14:33:29 -0500

    Setup OAuth structure

--- secure-server/src/server.js
+++ secure-server/src/server.js
@@ -11,9 +11,28 @@ const app = express();
 app.use(helmet());
 const credentials = { key: privateKey, cert: certificate };
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '../index.html'));
+function checkIsLoggedIn(req, res, next) {
+	const isLoggedIn = true;
+	if (!isLoggedIn) {
+		res.status(400).send({
+			error: 'You are not logged in',
+		});
+	}
+	next();
+}
+
+app.get('/auth/google', (req, res) => {});
+app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', function (req, res) {
+	res.status(200).send('Your secret is 43!');
 });
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
 const server = https.createServer(credentials, app);
 
-server.listen(3030, () => console.log('App is serving on port 3030'));
+server.listen(8080, () => console.log('App is serving on port 8080'));

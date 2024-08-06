4ca6d8842e45346aebb5371fe4cedd23a608de0e
aavetisyanit <aavetisyanit@gmail.com>
2024-08-06 16:11:51 -0500

    Add controller logic to create user

--- node-jwt-auth/controllers/authControllers.js
+++ node-jwt-auth/controllers/authControllers.js
@@ -1,14 +1,23 @@
+const Users = require('../models/Users');
+
 module.exports.login_get = (req, res) => {
 	res.render('login');
 };
 
 module.exports.login_post = (req, res) => {
-	res.send('Login');
+	res.json('Login');
 };
 
 module.exports.signup_get = (req, res) => {
 	res.render('signup');
 };
-module.exports.signup_post = (req, res) => {
-	res.send('signup post');
+module.exports.signup_post = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		const newUser = await Users.create({ email, password });
+		res.status(201).json(newUser);
+	} catch (err) {
+		console.log('User is not created. Error:', err);
+		res.status(404).json('User is not created');
+	}
 };

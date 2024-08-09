f538eda99b12cd80f743f665308150cd3fae0107
aavetisyanit <aavetisyanit@gmail.com>
2024-08-08 23:14:32 -0500

    Add mongoDB validations

--- node-jwt-auth/models/Users.js
+++ node-jwt-auth/models/Users.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
 const userSchema = mongoose.Schema({
 	email: {
 		type: String,
-		required: true,
+		required: [true, 'Email is required'],
 		unique: true,
 		lowercase: true,
+		validate: [isEmail, 'Please, enter a valid email'],
 	},
 	password: {
 		type: String,
-		required: true,
-		maxLength: 6,
+		required: [true, 'Password is required'],
+		minlength: [6, 'Password must be at list 6 characters'],
 	},
 });
 

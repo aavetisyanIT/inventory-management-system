0a3d46c49c3694bdcc533d06a02ccbf83e6ed2a2
aavetisyanit <aavetisyanit@gmail.com>
2024-08-08 23:14:53 -0500

    WIP handle validation function

--- node-jwt-auth/controllers/authControllers.js
+++ node-jwt-auth/controllers/authControllers.js
@@ -1,4 +1,15 @@
 const Users = require('../models/Users');
+const util = require('util');
+
+function handleErrors(err) {
+	const errors = { email: '', password: '' };
+	if (err.message.includes('user validation failed')) {
+		console.log(err);
+		console.log(util.inspect(err, false, null, true /* enable colors */));
+	}
+
+	return errors;
+}
 
 module.exports.login_get = (req, res) => {
 	res.render('login');
@@ -17,7 +28,7 @@ module.exports.signup_post = async (req, res) => {
 		const newUser = await Users.create({ email, password });
 		res.status(201).json(newUser);
 	} catch (err) {
-		console.log('User is not created. Error:', err);
-		res.status(404).json('User is not created');
+		const errors = handleErrors(err);
+		res.status(404).json(errors);
 	}
 };

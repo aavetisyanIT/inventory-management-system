336e1019d8b725129d9b04b58fb9b3fbd40ec9bf
aavetisyanit <aavetisyanit@gmail.com>
2024-08-09 11:16:41 -0500

    Hash password before updating DB

--- node-jwt-auth/models/Users.js
+++ node-jwt-auth/models/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
 	email: {
@@ -14,6 +15,12 @@ const userSchema = mongoose.Schema({
 		required: [true, 'Password is required'],
 		minlength: [6, 'Password must be at list 6 characters'],
 	},
+});
+
+userSchema.pre('save', function (next) {
+	const salt = bcrypt.genSaltSync(10);
+	this.password = bcrypt.hashSync(this.password, salt);
+	next();
 });
 
 module.exports = mongoose.model('user', userSchema);

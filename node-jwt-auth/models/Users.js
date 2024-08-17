bdeceef63b054e9cdeeb4d2aa76204915db5b9c6
aavetisyanIT <aavetisyanIT@gmail.com>
2024-08-16 22:49:14 -0500

    Test login logic

--- node-jwt-auth/models/Users.js
+++ node-jwt-auth/models/Users.js
@@ -1,26 +1,39 @@
-const mongoose = require('mongoose');
-const { isEmail } = require('validator');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = mongoose.Schema({
-	email: {
-		type: String,
-		required: [true, 'Email is required'],
-		unique: true,
-		lowercase: true,
-		validate: [isEmail, 'Please, enter a valid email'],
-	},
-	password: {
-		type: String,
-		required: [true, 'Password is required'],
-		minlength: [6, 'Password must be at list 6 characters'],
-	},
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please, enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at list 6 characters"],
+  },
 });
 
-userSchema.pre('save', function (next) {
-	const salt = bcrypt.genSaltSync(10);
-	this.password = bcrypt.hashSync(this.password, salt);
-	next();
+userSchema.pre("save", function (next) {
+  const salt = bcrypt.genSaltSync(10);
+  this.password = bcrypt.hashSync(this.password, salt);
+  next();
 });
 
-module.exports = mongoose.model('user', userSchema);
+userSchema.statics.login = async function (email, password) {
+  const user = await this.findOne({ email });
+
+  if (user) {
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      return user;
+    }
+    throw Error("invalid password");
+  }
+  throw Error("invalid email");
+};
+
+module.exports = mongoose.model("user", userSchema);

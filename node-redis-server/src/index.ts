776037d92a8505faab2d9e13670f24c29d92ec8c
aavetisyanIT <aavetisyanIT@gmail.com>
2024-08-22 17:02:31 -0500

    Initial setup, express app with typescript, axios and nodemon

---
+++ node-redis-server/src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Test home route");
+});
+
+app.listen(port, () => console.log(`Server is listening on port: ${port}`));

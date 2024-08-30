b24d551fee9def83cffdaafad88d63b290b87937
aavetisyanIT <aavetisyanIT@gmail.com>
2024-08-30 16:54:14 -0500

    Set up express app

--- mailbag/server/src/main.ts
+++ mailbag/server/src/main.ts
@@ -1,1 +1,9 @@
-export default {};
+import express, { Express } from "express";
+import path from "path";
+
+const app: Express = express();
+
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+
+app.listen(8080);

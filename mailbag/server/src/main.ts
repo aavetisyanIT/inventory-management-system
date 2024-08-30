29db6358c7de12c4902b8f501c9de83c1a0d7fe5
aavetisyanIT <aavetisyanIT@gmail.com>
2024-08-30 17:19:15 -0500

    Add middleware that sets up CORS headers

--- mailbag/server/src/main.ts
+++ mailbag/server/src/main.ts
@@ -1,9 +1,18 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, NextFunction } from "express";
 import path from "path";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept",
+  );
+  next();
+});
 
 app.listen(8080);

ee48a81a9df7709b7fc176051209ce49a9ad9ac3
aavetisyanIT <aavetisyanIT@gmail.com>
2024-09-03 16:33:07 -0500

    Define logic to send message

--- mailbag/server/src/SMTP.ts
+++ mailbag/server/src/SMTP.ts
@@ -8,6 +8,24 @@ export class Worker {
   constructor(inServerInfo: IServerInfo) {
     Worker.serverInfo = inServerInfo;
   }
+  public setMessage(options: SendMailOptions): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const transporter: Mail = nodemailer.createTransport(
+        Worker.serverInfo.smtp,
+      );
+      transporter.sendMail(
+        options,
+        (error: Error | null, info: SentMessageInfo) => {
+          console.log("sendMail info", info);
+          if (error) {
+            reject(error);
+            return;
+          }
+          resolve("");
+        },
+      );
+    });
+  }
 }
 
 export {};

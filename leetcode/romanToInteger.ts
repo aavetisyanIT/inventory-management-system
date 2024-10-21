9e209fa15c92adedaa43391013b6336847a6108d
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-21 14:00:58 -0500

    Refactor using forEach

--- leetcode/romanToInteger.ts
+++ leetcode/romanToInteger.ts
@@ -12,21 +12,13 @@ function romanToInt(s: string): number {
   const romanNumberArr = s.split("");
   let result = 0;
 
-  for (let i = romanNumberArr.length - 1; i >= 0; i--) {
-    if (i === romanNumberArr.length - 1) {
-      result += map[romanNumberArr[i]];
+  romanNumberArr.forEach((romanNum, i) => {
+    if (map[romanNum] < map[romanNumberArr[i + 1]]) {
+      result -= map[romanNum];
     } else {
-      const currentRomanNum = romanNumberArr[i];
-      const currentNum = map[currentRomanNum];
-      const nextRomanNum = romanNumberArr[i + 1];
-      const nextNum = map[nextRomanNum];
-      if (currentNum < nextNum) {
-        result -= currentNum;
-      } else {
-        result += currentNum;
-      }
+      result += map[romanNum];
     }
-  }
+  });
 
   return result;
 }

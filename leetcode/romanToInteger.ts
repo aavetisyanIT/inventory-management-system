e994fe0701d13df7130608b01cc1179d70b8a8f4
aavetisyanIT <aavetisyanIT@gmail.com>
2024-10-21 14:40:49 -0500

    Use while loop to solve remanToInt algorithm

--- leetcode/romanToInteger.ts
+++ leetcode/romanToInteger.ts
@@ -7,18 +7,27 @@ function romanToInt(s: string): number {
     C: 100,
     D: 500,
     M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
   };
 
   const romanNumberArr = s.split("");
   let result = 0;
-
-  romanNumberArr.forEach((romanNum, i) => {
-    if (map[romanNum] < map[romanNumberArr[i + 1]]) {
-      result -= map[romanNum];
+  let i = 0;
+  while (i < romanNumberArr.length) {
+    const exceptionRomanNum = romanNumberArr[i] + romanNumberArr[i + 1];
+    const exceptionNumber = map[exceptionRomanNum];
+    if (exceptionNumber) {
+      result += exceptionNumber;
+      i = i + 2;
     } else {
-      result += map[romanNum];
+      result += map[romanNumberArr[i]];
+      i++;
     }
-  });
-
+  }
   return result;
 }

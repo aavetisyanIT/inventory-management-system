5956863adc708cd999b67cc3a7b7feaf6359c7ea
aavetisyanIT <aavetisyanIT@gmail.com>
2024-11-06 17:04:58 -0600

    Refactor using reduce method

--- leetcode/reverseArrOfWords.ts
+++ leetcode/reverseArrOfWords.ts
@@ -1,9 +1,10 @@
 function reverseWords(s: string): string {
   const resultArr: string[] = [];
   for (let word of s.split(" ")) {
-    const wordArr = word.split("");
-    wordArr.reverse();
-    resultArr.push(wordArr.join(""));
+    const reversedWord = word
+      .split("")
+      .reduce((prev, current) => current + prev);
+    resultArr.push(reversedWord);
   }
   return resultArr.join(" ");
 }

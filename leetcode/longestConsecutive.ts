cad0cb78063bc886d7876e3962d78bdd7b31c265
aavetisyanIT <aavetisyanIT@gmail.com>
2024-12-05 15:25:02 -0600

    Refactor logic to improve time complexity

--- leetcode/longestConsecutive.ts
+++ leetcode/longestConsecutive.ts
@@ -1,17 +1,22 @@
 function longestConsecutive(nums: number[]): number {
-  if (!nums.length) return 0;
-  const set = new Set(nums.sort((a, b) => a - b));
-  const sortedArr = Array.from(set);
-  const resultArr: number[] = [];
-
-  let count = 1;
-  for (let i = 0; i < sortedArr.length; i++) {
-    if (sortedArr[i + 1] - sortedArr[i] === 1) {
-      count++;
-    } else {
-      resultArr.push(count);
-      count = 1;
+  const numsSet: Set<number> = new Set(nums);
+  const resultMap: Map<number, number> = new Map();
+  for (let number of numsSet) {
+    if (!numsSet.has(number - 1)) {
+      let sequenceCount = 1;
+      while (numsSet.has(number + sequenceCount)) {
+        sequenceCount++;
+      }
+      resultMap.set(number, sequenceCount);
     }
   }
-  return resultArr.sort((a, b) => a - b)[resultArr.length - 1];
+  let result: number = 0;
+  for (let entry of resultMap) {
+    const sequence = entry[1];
+    if (result < sequence) {
+      result = sequence;
+    }
+  }
+
+  return result;
 }

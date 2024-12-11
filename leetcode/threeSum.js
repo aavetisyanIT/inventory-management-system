fba8bf06ee180eea67fc22c25e9487b0ab06d9c0
aavetisyanIT <aavetisyanIT@gmail.com>
2024-12-11 15:33:45 -0600

    Fix logical issue

--- leetcode/threeSum.js
+++ leetcode/threeSum.js
@@ -9,7 +9,7 @@ class Solution {
 
     for (let i = 0; i < nums.length; i++) {
       if (nums[i] > 0) break;
-      if (i > 0 && nums[i] === nums[i]) continue;
+      if (i > 0 && nums[i] === nums[i - 1]) continue;
 
       let l = i + 1;
       let r = nums.length - 1;

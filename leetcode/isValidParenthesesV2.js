cc4daab386a8cf1d072c9e7e281ec16f99c2b0c1
aavetisyanIT <aavetisyanIT@gmail.com>
2024-12-13 17:16:32 -0600

    Refactor code and remove redundant if statement

--- leetcode/isValidParenthesesV2.js
+++ leetcode/isValidParenthesesV2.js
@@ -1,27 +1,24 @@
-class Solution {
-  /**
-   * @param {string} s
-   * @return {boolean}
-   */
-  isValid(s) {
-    const stack = [];
-    const map = {
-      "{": "}",
-      "(": ")",
-      "[": "]",
-    };
-    for (let p of s) {
-      if (p in map) {
-        stack.push(p);
-      } else {
-        if (stack.length === 0) return false;
-        if (map[stack[stack.length - 1]] === p) {
-          stack.pop();
-        } else if (map[stack[stack.length - 1]] !== p) {
-          return false;
-        }
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  const stack = [];
+  const map = {
+    "{": "}",
+    "(": ")",
+    "[": "]",
+  };
+  for (let p of s) {
+    if (p in map) {
+      stack.push(p);
+    } else {
+      if (stack.length === 0) return false;
+      if (map[stack.pop()] === p) {
+        continue;
       }
+      return false;
     }
-    return stack.length === 0;
   }
-}
+  return stack.length === 0;
+};

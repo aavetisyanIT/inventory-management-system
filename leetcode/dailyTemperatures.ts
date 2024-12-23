fe6dc8ad530f7e9516ddd97a47bdd43b76fe9563
aavetisyanIT <aavetisyanIT@gmail.com>
2024-12-23 14:37:48 -0600

    Optimize dailyTemperature method

--- leetcode/dailyTemperatures.ts
+++ leetcode/dailyTemperatures.ts
@@ -1,15 +1,13 @@
 function dailyTemperatures(temperatures: number[]): number[] {
   const answer: number[] = new Array(temperatures.length).fill(0);
-  const stack: number[] = [];
+  const stack: number[] = [0];
 
   for (let i = 0; i < temperatures.length; i++) {
-    const currentTemp = temperatures[i];
-    while (
-      stack.length > 0 &&
-      currentTemp > temperatures[stack[stack.length - 1]]
-    ) {
-      const lastStackTemp = stack.pop();
-      answer[i] = i - stack[i];
+    while (temperatures[i] > temperatures[stack[stack.length - 1]]) {
+      const index = stack.pop();
+      if (index !== undefined) {
+        answer[index] = i - index;
+      }
     }
     stack.push(i);
   }

452dfaee4ecc654cd8ddecc55607771db355462b
aavetisyanIT <aavetisyanIT@gmail.com>
2025-12-17 16:57:02 -0600

    Sort arrays with insertion logic

---
+++ compute_science/insertionSort.js
@@ -0,0 +1,23 @@
+let testArr = [3, 2, 1, 12, 0, -2, 4, 2];
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentValue = arr[i];
+    let j = i - 1;
+    console.log("arr: ", arr);
+    console.log("currentValue: ", currentValue);
+    console.log("j: ", j);
+    console.log("arr[j]: ", arr[j]);
+
+    while (arr[j] > currentValue && j >= 0) {
+      console.log("arr[j + 1]: ", arr[j + 1], j + 1);
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = currentValue;
+  }
+  return arr;
+}
+
+insertionSort(testArr);
+console.log("sorted:", testArr);

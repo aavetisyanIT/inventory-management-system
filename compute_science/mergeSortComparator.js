f3c70939cbab35bdcad9a103bab82bd3b61d60a7
aavetisyanIT <aavetisyanIT@gmail.com>
2025-12-18 13:49:39 -0600

    Sort array by merging with comparator

---
+++ compute_science/mergeSortComparator.js
@@ -0,0 +1,25 @@
+function merge(arr1, arr2, comparator) {
+  if (typeof comparator !== "function") comparator = (a, b) => a - b;
+
+  const result = new Array(arr1.length + arr2.length);
+  let arr1Index = 0;
+  let arr2Index = 0;
+  let resultIndex = 0;
+
+  while (arr1Index < arr1.length && arr2Index < arr2.length) {
+    if (comparator(arr1[arr1Index], arr2[arr2Index]) <= 0) {
+      result[resultIndex++] = arr1[arr1Index++];
+    } else {
+      result[resultIndex++] = arr2[arr2Index++];
+    }
+  }
+
+  while (arr1Index < arr1.length) {
+    result[resultIndex++] = arr1[arr1Index++];
+  }
+  while (arr2Index < arr2.length) {
+    result[resultIndex++] = arr2[arr2Index++];
+  }
+
+  return result;
+}

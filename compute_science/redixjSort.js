d448a7559376ab1563307e2d8c8a2768a16bc3cc
aavetisyanIT <aavetisyanIT@gmail.com>
2026-01-05 18:05:34 -0600

    Initial radix sort implementation

--- compute_science/redixjSort.js
+++ compute_science/redixjSort.js
@@ -18,3 +18,35 @@ function mostDigits(numArr) {
 
   return result;
 }
+
+function radixSort(numArr) {
+  const mostDigitsNum = mostDigits(numArr);
+
+  for (let k = 0; k < mostDigitsNum; k++) {
+    const map = {
+      0: [],
+      1: [],
+      2: [],
+      3: [],
+      4: [],
+      5: [],
+      6: [],
+      7: [],
+      8: [],
+      9: [],
+    };
+
+    for (let num of numArr) {
+      const currentNumKthDigit = getDigit(num, k);
+      map[currentNumKthDigit].push(num);
+    }
+    numArr = [];
+
+    for (let key in map) {
+      numArr = [...numArr, ...map[key]];
+    }
+  }
+  return numArr;
+}
+
+console.log(radixSort([10, 7, 1, 4, 5678, 2734]));

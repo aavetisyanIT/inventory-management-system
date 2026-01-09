a12816f6ff405f3c2467e08069878bf5a7f8bcba
aavetisyanIT <aavetisyanIT@gmail.com>
2026-01-09 16:29:33 -0600

    Implement mergeSort algorithm

--- compute_science/sortingAlgorithmMethods.js
+++ compute_science/sortingAlgorithmMethods.js
@@ -51,10 +51,44 @@ function insertionSort(nums) {
 
   return nums;
 }
-insertionSort(unsortedArr);
+// insertionSort(unsortedArr);
 
-function mergeSort() {}
+function merge(arr1, arr2) {
+  const result = new Array(arr1.length + arr2.length);
+
+  let arr1Index = 0;
+  let arr2Index = 0;
+  let resultIndex = 0;
+
+  while (arr1Index < arr1.length && arr2Index < arr2.length) {
+    if (arr1[arr1Index] <= arr2[arr2Index]) {
+      result[resultIndex++] = arr1[arr1Index++];
+    } else {
+      result[resultIndex++] = arr2[arr2Index++];
+    }
+  }
+
+  while (arr1Index < arr1.length) {
+    result[resultIndex++] = arr1[arr1Index++];
+  }
+
+  while (arr2Index < arr2.length) {
+    result[resultIndex++] = arr2[arr2Index++];
+  }
+
+  return result;
+}
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, middle));
+  const right = mergeSort(arr.slice(middle));
+
+  return merge(left, right);
+}
 function quickSort() {}
 
-console.log(unsortedArr);
-console.log(swapCount);
+console.log(mergeSort(unsortedArr));
+// console.log(swapCount);

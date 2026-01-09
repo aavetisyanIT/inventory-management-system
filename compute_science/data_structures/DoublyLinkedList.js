701e99b3fa9e70a98fe03a179145c75f0ffa037c
aavetisyanIT <aavetisyanIT@gmail.com>
2026-01-09 16:51:50 -0600

    Implement logic to push value to DLL

--- compute_science/data_structures/DoublyLinkedList.js
+++ compute_science/data_structures/DoublyLinkedList.js
@@ -12,4 +12,31 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newTail = new Node(val);
+
+    if (!this.head) {
+      this.head = newTail;
+      this.tail = newTail;
+      this.length++;
+      return this;
+    }
+
+    this.tail.next = newTail;
+    newTail.prev = this.tail;
+    this.tail = newTail;
+    this.length++;
+
+    return this;
+  }
 }
+
+const myList = new DoublyLinkedList();
+myList.push(100);
+myList.push(201);
+myList.push(250);
+myList.push(350);
+myList.push(999);
+
+console.log(myList);

58516dcaeaabdbf23da1d8338a6074d7d5d81141
aavetisyanIT <aavetisyanIT@gmail.com>
2026-02-02 17:42:14 -0600

    Implement linear search logic

---
+++ compute_science/linear_search.py
@@ -0,0 +1,8 @@
+def linear_search(arr, num):
+  for index, currentNum in enumerate(arr):
+    if num == currentNum:
+      return index
+  return -1
+
+testArr = [1,2,5,6,7]
+print(linear_search(testArr, 5))

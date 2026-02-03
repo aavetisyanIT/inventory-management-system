8f9f8f4cb76bcdc67f9f93b7028f8677e7a750f5
aavetisyanIT <aavetisyanIT@gmail.com>
2026-02-03 17:07:14 -0600

    Implement binary search algorithm

--- compute_science/binary_search.py
+++ compute_science/binary_search.py
@@ -1,16 +1,18 @@
 import math
 
-def binary_search(arr, num, start = 0, end=None):
-  if end is None:
-    end = len(arr)  # Use the actual length of the list
-  if start >= end: return start
-  half_index = math.floor((start + end) /2)
-  print('AAA half_index: ', half_index)
-  if num < arr[half_index]:
-    binary_search(arr, num, start, half_index)
-  else:
-    binary_search(arr, num, half_index, end)
-  return -1
+def binary_search(arr, num):
+  left = 0
+  right = len(arr)
+  while left < right:
+    mid_index = math.floor((left + right)/2)
+    if num == arr[mid_index]:
+      return mid_index
+    if num < arr[mid_index]:
+      right = mid_index - 1
+    else:
+      left = mid_index + 1
+      
+  return -1 
 
-testArr = [1,2,5,6,7]
-print(binary_search(testArr, 2))
+testArr = [1,2,5,6,7, 10, 100, 144, 49009]
+print(binary_search(testArr, 144))

d11ce4ca8761f8bdfd1003abc1c1ff93d76161d4
aavetisyanIT <aavetisyanIT@gmail.com>
2026-02-04 17:29:27 -0600

    Enhance performance with swap flag

--- compute_science/bubble_sort.py
+++ compute_science/bubble_sort.py
@@ -1,17 +1,22 @@
 def swap(left, right, arr):
-  temp = arr[left]
-  arr[left] = arr[right]
-  arr[right] = temp
+    temp = arr[left]
+    arr[left] = arr[right]
+    arr[right] = temp
 
-# Todo: add isSwap optimization 
+
+# Todo: add isSwap optimization
 def bubble_sort(arr):
-  for i in range(0, len(arr) -1):
-    for j in range(0, len(arr) - i -1):
-        if j >= len(arr) - 1: continue 
-        if arr[j] > arr[j + 1]:
-          swap(j, j +1, arr)
+    for i in range(0, len(arr)):
+        swapped = False
+        for j in range(0, len(arr) - i - 1):
+            if arr[j] > arr[j + 1]:
+                swap(j, j + 1, arr)
+                swapped = True
+        if not swapped:
+            break
 
-  return arr
+    return arr
 
-testArr = [4,6,1,2,90, 55, -1, -11]
+
+testArr = [64, 34, 25, 12, 22, 11, 90]
 print(bubble_sort(testArr))

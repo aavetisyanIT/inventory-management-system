e318c8312c60aa9141a034cf07fee01b382781cb
aavetisyanIT <aavetisyanIT@gmail.com>
2026-02-06 17:24:35 -0600

    Implement insertion sorting

--- compute_science/insertion_sort_imp.py
+++ compute_science/insertion_sort_imp.py
@@ -1,29 +1,15 @@
-def swap(left, right, arr):
-    temp = arr[left]
-    arr[left] = arr[right]
-    arr[right] = temp
-
-
 def insertion_sort(arr: list[int]):
     for i in range(1, len(arr)):
-        swapIndex = i
         val = arr[i]
-        for j in range(0, i):
-            # print("inner loop j: ", j, arr[j])
-            # print("inner loop i: ", i, arr[i])
+        swapIndex = i
+        while swapIndex > 0 and val < arr[swapIndex - 1]:
+            arr[swapIndex] = arr[swapIndex - 1]
+            swapIndex -= 1
 
-            if arr[j] > arr[i]:
-                swapIndex = j
-                arr[j] = arr[j - 1]
-            else:
-                break
-        # print("outer loop swapIndex: ", swapIndex, arr[swapIndex])
-        # print(f"swapping swapIndex:{swapIndex}({arr[swapIndex]}) with i: {i}({arr[i]})")
         arr[swapIndex] = val
         print(arr)
 
 
 testArr = [100, -6, 1, 2, 90, 55, -1, -11]
-#          j     i
 insertion_sort(testArr)
 print(testArr)

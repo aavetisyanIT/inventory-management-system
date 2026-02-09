e5b6f7209864275b4744bd2a4c611f32d7b41da4
aavetisyanIT <aavetisyanIT@gmail.com>
2026-02-09 16:17:45 -0600

    Refactor merge logic

--- compute_science/merge.py
+++ compute_science/merge.py
@@ -1,28 +1,19 @@
 def merge(arr1: list[int], arr2: list[int]):
-    result: list[int] = [0] * (len(arr1) + len(arr2))
-    resultIndex = 0
+    result: list[int] = []
     arr1Index = 0
     arr2Index = 0
 
     while arr1Index < len(arr1) and arr2Index < len(arr2):
 
         if arr1[arr1Index] <= arr2[arr2Index]:
-            result[resultIndex] = arr1[arr1Index]
-            resultIndex += 1
+            result.append(arr1[arr1Index])
             arr1Index += 1
         else:
-            result[resultIndex] = arr2[arr2Index]
-            resultIndex += 1
+            result.append(arr2[arr2Index])
             arr2Index += 1
 
-    while len(arr1) > arr1Index:
-        result[resultIndex] = arr1[arr1Index]
-        resultIndex += 1
-        arr1Index += 1
-    while len(arr2) > arr2Index:
-        result[resultIndex] = arr2[arr2Index]
-        resultIndex += 1
-        arr2Index += 1
+    result.extend(arr1[arr1Index:])
+    result.extend(arr2[arr2Index:])
 
     return result
 
